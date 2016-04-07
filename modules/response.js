85ff28bac2bb744b662465ad93857dd4732e6139
Ichiroku <deathrukia@gmail.com>
2016-04-07 22:23:39 +0200

    sempai i love you

--- modules/response.js
+++ modules/response.js
@@ -1,5 +1,6 @@
 var responses = require("../responses.js");
 var util = require("../util.js");
+var gd = require('node-gd');
 
 module.exports = {
     moduleName: "Response",
@@ -13,6 +14,34 @@ module.exports = {
             //todo
             Bot.discord.reply(message, responses.get("SEMPAI_FUCKYOU").format({author: message.author.id}));
         }
+      });
+      Bot.addCommand({
+          name: "RESPONSE_LOVE",
+          command: /i love you/,
+          sample: "sempai i love you",
+          description: "Show sempai some love",
+          action: function(m) {
+              var img = gd.createSync(500, 449);
+              gd.openFile('/assets/chitoge_love.png', function(err, img) {
+                if (err) {
+                  throw err;
+                }
+                var txtColor = img.colorAllocate(255, 255, 255);
+                var fontPath = '/assets/wildwordsbold.ttf';
+                img.stringFT(txtColor, fontPath, 24, 0, 35, 400, m.author.username);
+                img.saveFile('/saved/' + m.author.username + ".png", function(err) {
+                    if (err) {
+                      throw err;
+                    }
+                    
+                    Bot.discord.sendFile(m.channel, "/saved/" + m.author.username + ".png", "love.png", function(err, message) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                }.bind(m));
+            }.bind(m));
+          }
       });
     }
 }
