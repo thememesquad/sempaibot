0c28fb12e96ece0023bbeee10836b1935dcd7855
Ichiroku <deathrukia@gmail.com>
2016-04-07 22:46:31 +0200

    testing

--- modules/response.js
+++ modules/response.js
@@ -21,22 +21,24 @@ module.exports = {
           sample: "sempai i love you",
           description: "Show sempai some love",
           action: function(m) {
-              var img = gd.createSync(500, 449);
               gd.openFile('/assets/chitoge_love.png', function(err, img) {
                 if (err) {
-                  throw err;
+                  console.log("Something went wrong opening file");
+                  return;
                 }
                 var txtColor = img.colorAllocate(255, 255, 255);
                 var fontPath = '/assets/wildwordsbold.ttf';
                 img.stringFT(txtColor, fontPath, 24, 0, 35, 400, m.author.username);
                 img.saveFile('/saved/' + m.author.username + ".png", function(err) {
                     if (err) {
-                      throw err;
+                      console.log("Something went wrong saving file");
+                  return;
                     }
                     
                     Bot.discord.sendFile(m.channel, "/saved/" + m.author.username + ".png", "love.png", function(err, message) {
                         if (err) {
-                            throw err;
+                            console.log("Something went wrong sending file");
+                            return;
                         }
                     });
                 }.bind(m));
