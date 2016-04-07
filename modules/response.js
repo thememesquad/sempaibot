6d472662fbac4207df2ed130b45546aab5cd5b02
RukiaXi <deathrukia@gmail.com>
2016-04-07 15:46:34 +0200

    Create response.js
    Added fuckyou

---
+++ modules/response.js
@@ -0,0 +1,18 @@
+var responses = require("../responses.js");
+var util = require("../util.js");
+
+module.exports = {
+    moduleName: "Response",
+    load: function(Bot) {
+      Bot.addCommand({
+        name: "RESPONSE_FUCKYOU",
+        command: /fuck you/,
+        sample: "sempai fuck you",
+        description: "Scold sempai",
+        action: function(message){
+            //todo
+            Bot.discord.reply(m, response.get("SEMPAI_FUCKYOU").format({author: m.author.id});
+        }
+      });
+    }
+}
