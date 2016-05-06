528a54b57dc17a6967afcb6359dd2e8b8a96bd5b
Ichiroku <deathrukia@gmail.com>
2016-05-06 22:57:44 +0200

    config :D

---
+++ config.js
@@ -0,0 +1,7 @@
+//----- Config
+module.exports = {
+	user: "info@semwong.com",
+	pass: "0nlyS3mKn0ws",
+	server: "https://discord.gg/0jJ7kVQNcmLdY6aO",
+	osuapi: "a8f348302f1c7ecc7167a0a1c80233a69171974a"
+};
