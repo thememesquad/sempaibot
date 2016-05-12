8fd5256d6cbd450f05220add3e7022f80840b596
Calvin Baart <calsmurf2904@outlook.com>
2016-05-12 02:47:06 +0200

    Started working on making the reminder syntax more flexible (relative time, less syntax etc.)

--- modules/reminders.js
+++ modules/reminders.js
@@ -47,14 +47,27 @@ class RemindersModule extends IModule
         });
         
         this.add_command({
-            regex: /^remind (.*) to (.*) at (.{4,})/i,
+            regex: [
+                /^remind (\S+)(?:(?:\s+)to)?(?:\s+)([^0-9]+)(?:at|in|after)(?:\s+)(.{4,})/i,
+                /^remind (\S+)(?:(?:\s+)to)?(?:\s+)([^0-9]+)(?:\s+)(.{4,})/i
+            ],
             sample: "sempai remind __*name*__  to __*reminder*__  at __*time*__",
             description: "Send yourself (or someone else) a reminder at a given timestamp. (name should be me when referring to yourself) (Timezone is set to the timezone of your discord server).",
             permission: null,
             global: false,
             
             execute: this.handle_remind
-        })
+        });
+        
+        /*console.log("5 minutes: " + this.parse_timestring("5 minutes"));
+        console.log("1 hour: " + this.parse_timestring("1 hour"));
+        console.log("1 year: " + this.parse_timestring("1 year"));
+        console.log("19:30: " + this.parse_timestring("19:30"));
+        console.log("tomorrow: " + this.parse_timestring("tomorrow"));
+        console.log("next week: " + this.parse_timestring("next week"));
+        console.log("2 weeks: " + this.parse_timestring("2 weeks"));
+        console.log("tuesday: " + this.parse_timestring("tuesday"));
+        console.log("saturday: " + this.parse_timestring("saturday"));*/
     }
     
     handle_list_reminders(message)
@@ -95,6 +108,194 @@ class RemindersModule extends IModule
         return this.bot.respond(message, responses.get("LIST_REMINDERS").format({author: message.author.id, response: response}));
     }
     
+    parse_timestring(str)
+    {
+        str = str.toLowerCase();
+        
+        var regex = /(\S+)(?:\s+)(\S+)/i;
+        
+        var time = "";
+        var currentDate = new Date();
+        
+        var day_func = function(target){
+            var current = currentDate.getDay();
+            
+            if(current == target)
+                return currentDate;
+                
+            var num = 0;
+            if(current > target)
+                num = ((target + 6) - current) + 1;
+            else
+                num = target - current;
+                
+            return (new Date(currentDate.getTime() + (num * 86400000)));
+        };
+        
+        switch(str)
+        {
+            case "monday":
+            {
+                return day_func(1);
+            }
+            
+            case "tuesday":
+            {
+                return day_func(2);
+            }
+            
+            case "wednesday":
+            {
+                return day_func(3);
+            }
+            
+            case "thursday":
+            {
+                return day_func(4);
+            }
+            
+            case "friday":
+            {
+                return day_func(5);
+            }
+            
+            case "saturday":
+            {
+                return day_func(6);
+            }
+            
+            case "sunday":
+            {
+                return day_func(0);
+            }
+            
+            case "tomorrow":
+            {
+                return (new Date(currentDate.getTime() + 86400000));
+            }
+        }
+        
+        var match = regex.exec(str);
+        if(match !== null)
+        {
+            switch(match[2])
+            {
+                case "second":
+                case "seconds":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        console.log("Unknown second: " + match[1]);
+                        return currentDate;
+                    }
+                    
+                    return (new Date(currentDate.getTime() + (num * 1000)));
+                }
+                
+                case "minute":
+                case "minutes":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        console.log("Unknown minute: " + match[1]);
+                        return currentDate;
+                    }
+                    
+                    return (new Date(currentDate.getTime() + (num * 60000)));
+                }
+                
+                case "hour":
+                case "hours":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        console.log("Unknown hour: " + match[1]);
+                        return currentDate;
+                    }
+                    
+                    return (new Date(currentDate.getTime() + (num * 3600000)));
+                }
+                
+                case "day":
+                case "days":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        console.log("Unknown day: " + match[1]);
+                        return currentDate;
+                    }
+                    
+                    return (new Date(currentDate.getTime() + (num * 86400000)));
+                }
+                
+                case "week":
+                case "weeks":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        switch(match[1])
+                        {
+                            case "next":
+                                num = 1;
+                                break;
+                                
+                            default:
+                                console.log("Unknown week: " + match[1]);
+                                return currentDate;
+                        }
+                    }
+                    
+                    return (new Date(currentDate.getTime() + ((num * 7) * 86400000)));
+                }
+                
+                case "month":
+                case "months":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        console.log("Unknown month: " + match[1]);
+                        return currentDate;
+                    }
+                    
+                    var month = (currentDate.getMonth() + num) % 12;
+                    time = (month + 1) + "-" + currentDate.getDate() + "-" + currentDate.getFullYear() + " " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+                    
+                    return new Date(time);
+                }
+                
+                case "year":
+                case "years":
+                {
+                    var num = parseInt(match[1]);
+                    if(isNaN(num))
+                    {
+                        console.log("Unknown year: " + match[1]);
+                        return currentDate;
+                    }
+                    
+                    var year = currentDate.getFullYear() + num;
+                    time = (currentDate.getMonth() + 1) + "-" + currentDate.getDate() + "-" + year + " " + currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+                    
+                    return new Date(time);
+                }
+            }
+        }
+        
+        time = str;
+        if (time.split(" ").length == 1)
+        {
+            time = (currentDate.getMonth() + 1) + "-" + currentDate.getDate() + "-" + currentDate.getFullYear() + " " + time;
+        }
+        
+        return new Date(time);
+    }
+    
     handle_remind(message, name, reminder, time)
     {
         var who = false;
@@ -103,15 +304,9 @@ class RemindersModule extends IModule
             who = name;
         }
 
-        var info = reminder;
-
         var currentDate = new Date();
-        if (time.split(" ").length == 1)
-        {
-            time = (currentDate.getMonth() + 1) + "-" + currentDate.getDate() + "-" + currentDate.getFullYear() + " " + time;
-        }
-
-        var parsedtime = new Date(time);
+        var info = reminder.trim();
+        var parsedtime = this.parse_timestring(time);
 
         if (parsedtime < currentDate) 
         {
