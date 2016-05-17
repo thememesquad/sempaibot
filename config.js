e9b1f7db80f2303ff7f679522173a191a7228f9e
Calvin Baart <calsmurf2904@outlook.com>
2016-05-17 15:02:05 +0200

    Config changes.

--- config.js
+++ config.js
@@ -1,12 +1,18 @@
 //----- Config
 var process = require("process");
 module.exports = {
-	user: "info@semwong.com",
-	pass: "0nlyS3mKn0ws",
-	server: "https://discord.gg/0jJ7kVQNcmLdY6aO",
-	osuapi: "a8f348302f1c7ecc7167a0a1c80233a69171974a",
+	token: "MTgyMTE0MjE0ODU1NDQyNDMz.ChymlA.oWGl3VC4GgIN8MRREk7pmycdZjM",
+	osu_api: "a8f348302f1c7ecc7167a0a1c80233a69171974a",
+	osu_irc_username: "calsmurf2904",
+	osu_irc_password: "077bec43",
 	db_username: "admin",
 	db_password: "YMHthrNBafJd",
 	db_host: process.env.OPENSHIFT_MONGODB_DB_HOST,
-	db_port: process.env.OPENSHIFT_MONGODB_DB_PORT
+	db_port: process.env.OPENSHIFT_MONGODB_DB_PORT,
+	
+	superadmins: [
+		"110525719133577216",
+		"130959661502300160",
+		"130423011483320320"
+	]
 }; 
