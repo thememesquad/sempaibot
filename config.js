1ae28327642ab85c55faf3c9bd45f6530c86d5d5
Ichiroku <deathrukia@gmail.com>
2016-05-18 21:41:16 +0200

    fix

---
+++ config.js
@@ -0,0 +1,18 @@
+//----- Config
+var process = require("process");
+module.exports = {
+	token: "MTgyMTE0MjE0ODU1NDQyNDMz.ChymlA.oWGl3VC4GgIN8MRREk7pmycdZjM",
+	osu_api: "a8f348302f1c7ecc7167a0a1c80233a69171974a",
+	osu_irc_username: "calsmurf2904",
+	osu_irc_password: "077bec43",
+	db_username: "3feb9b1e6dd0754a8ff7abbb62a841af",
+	db_password: "YMHthrNBafJd",
+	db_host: "eu-1.evennode.com",
+	db_port: 27017,
+	
+	superadmins: [
+		"110525719133577216",
+		"130959661502300160",
+		"130423011483320320"
+	]
+}; 
