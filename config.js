88f21b3c8a5f55292e1388c9cd6941e7d4aeed8a
Sem Wong <deathrukia@gmail.com>
2016-08-17 21:37:56 +0200

    Removed bancho

--- config.js
+++ config.js
@@ -3,8 +3,6 @@ var process = require("process");
 module.exports = {
 	token: "MTgyMTE0MjE0ODU1NDQyNDMz.ChymlA.oWGl3VC4GgIN8MRREk7pmycdZjM",
 	osu_api: "a8f348302f1c7ecc7167a0a1c80233a69171974a",
-	osu_irc_username: "calsmurf2904",
-	osu_irc_password: "077bec43",
 	db_username: "3feb9b1e6dd0754a8ff7abbb62a841af",
 	db_password: "YMHthrNBafJd",
 	db_database: "3feb9b1e6dd0754a8ff7abbb62a841af",
