00d58f7f45184b3315df9a3794d29fef4b485f5d
Sem Wong <deathrukia@gmail.com>
2017-04-19 21:14:14 +0200

    Added changelog

--- src/changelog.js
+++ src/changelog.js
@@ -2,7 +2,9 @@ module.exports = {
     changelog: [
         [1, "Sempai is now more flexible with command syntax."],
         [1, "Fixed a bug where only the lowercase version of the calling word 'sempai' would work."],
-        [1, "Fixed a bug in the list permissions command."]
+        [1, "Fixed a bug in the list permissions command."],
+        [2, "We've reworked the way we announce scores by using the new Discord embeds! See the screenshot below to see how it looks like!"],
+        [2, "https://puu.sh/vpA6g/703727fee7.png"]
     ],
-    version: 1
+    version: 2
 };
