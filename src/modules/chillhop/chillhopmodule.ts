cda575491fa6e8706f34defacd036c60e3732b30
Unknown <deathrukia@gmail.com>
2017-11-15 23:57:30 +0100

    /Chillhop: Auto stash before merge of "sem/Chillhop" and "calvin/Typescript"

---
+++ src/modules/chillhop/chillhopmodule.ts
@@ -0,0 +1,22 @@
+import {
+    Command,
+    CommandDescription,
+    CommandPermission,
+    CommandProcessor,
+    CommandSample,
+    Cron,
+    DB,
+    ICronInterface,
+    IMessage,
+    IRequest,
+    IResponse,
+    LoadBalancer,
+    MessageID,
+    Module,
+    ModuleBase,
+    ModuleOptions,
+    PermissionManager,
+    PersonalityManager,
+    RoleType,
+    Server
+} from "../../core";
